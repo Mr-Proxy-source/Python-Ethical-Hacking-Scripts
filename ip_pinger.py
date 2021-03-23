021fa43844c1e9a2e17774b0aedf9e03ea84ad63
Mr.  プロキシ <80650301+Mr-Proxy-source@users.noreply.github.com>
2021-03-23 15:34:33 +0100

    Create ip_pinger.py

---
+++ ip_pinger.py
@@ -0,0 +1,18 @@
+import os 
+
+os.system('cls')
+
+print("****************************************************************")
+print("* ╔╦╗╦═╗  ╔═╗╦═╗╔═╗═╗ ╦╦ ╦  ╦╔═╗  ╔═╗╦ ╦╔═╗╔═╗╦╔═╔═╗╦═╗        *")
+print("* ║║║╠╦╝  ╠═╝╠╦╝║ ║╔╩╦╝╚╦╝  ║╠═╝  ║  ╠═╣║╣ ║  ╠╩╗║╣ ╠╦╝        *")
+print("* ╩ ╩╩╚═  ╩  ╩╚═╚═╝╩ ╚═ ╩   ╩╩    ╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝╩╚═        *")
+print("* Copyright of Mr.Proxy, 2021                                  *")
+print("* https://github.com/Mr-Proxy-source                           *")
+print("* https://www.youtube.com/c/MrProxy1                           *")
+print("****************************************************************")
+
+ip = input("Type IP for Ping: ")
+
+print('-' * 55)
+os.system('ping {}'.format(ip))
+print('-' * 55)
