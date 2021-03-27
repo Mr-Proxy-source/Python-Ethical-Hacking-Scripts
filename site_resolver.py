4da26c9867f3fa0a58aa41ca85f2e798d3577c53
Mr.  プロキシ <80650301+Mr-Proxy-source@users.noreply.github.com>
2021-03-28 00:55:16 +0100

    Update site_resolver.py

--- site_resolver.py
+++ site_resolver.py
@@ -1,22 +1,19 @@
-######################################
-#Copyright of Mr.Proxy, 2021         #
-#https://github.com/Mr-Proxy-source  #
-#https://www.youtube.com/c/MrProxy1  #
-######################################
+import socket
+import os
 
-import socket, os
-
+print("*****************************************************************")
+print("* ╔╦╗╦═╗  ╔═╗╦═╗╔═╗═╗ ╦╦ ╦                                      *")
+print("* ║║║╠╦╝  ╠═╝╠╦╝║ ║╔╩╦╝╚╦╝                                      *")
+print("* ╩ ╩╩╚═  ╩  ╩╚═╚═╝╩ ╚═ ╩                                       *")
+print("* Copyright of Mr.Proxy, 2021                                   *")
+print("* https://github.com/Mr-Proxy-source                            *")
+print("* https://www.youtube.com/c/MrProxy1                            *")
+print("*****************************************************************")
 
 while True:
-    print("*****************************************************************")
-    print("* ╔╦╗╦═╗  ╔═╗╦═╗╔═╗═╗ ╦╦ ╦                                      *")
-    print("* ║║║╠╦╝  ╠═╝╠╦╝║ ║╔╩╦╝╚╦╝                                      *")
-    print("* ╩ ╩╩╚═  ╩  ╩╚═╚═╝╩ ╚═ ╩                                       *")
-    print("* Copyright of Mr.Proxy, 2021                                   *")
-    print("* https://github.com/Mr-Proxy-source                            *")
-    print("* https://www.youtube.com/c/MrProxy1                            *")
-    print("*****************************************************************")
     site = input('Tipe hostname of site you want resolve: ')
     print("****************************************************************")
+
     print(socket.gethostbyname(site))
+
     print('-' * 55)
