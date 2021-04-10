3875816095d3515f90fe2104c95e3b589c13b8c1
Mr.  プロキシ <80650301+Mr-Proxy-source@users.noreply.github.com>
2021-04-10 10:44:45 +0200

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -3,12 +3,28 @@ import socket, os
 
 os.system('cls')
 
-###In this field where is tipe 'HERE YOUR IPV4 ADDRESS' tipe in there you IPV4 address ***DONT DELETE '' IF YOU DELETE IT SCRIPT WILL NOT DOING***
+###In this field where is tipe 'HERE YOUR IPV4 ADDRESS' tipe in there you IPV4 address ***DONT DELETE '' IF YOU DELETE IT SCRIPT WILL NOT DOING  
+
+print("""___  ___      ______                     ______          _   
+|  \/  |      | ___ \                    | ___ \        | |  
+| .  . |_ __  | |_/ / __ _____  ___   _  | |_/ /__  _ __| |_ 
+| |\/| | '__| |  __/ '__/ _ \ \/ / | | | |  __/ _ \| '__| __|
+| |  | | |    | |  | | | (_) >  <| |_| | | | | (_) | |  | |_ 
+\_|  |_/_|    \_|  |_|  \___/_/\_\\__, | \_|  \___/|_|   \__|
+                                   __/ |                     
+                                  |___/                      
+ _____                                 
+/  ___|                                
+\ `--.  ___ __ _ _ __  _ __   ___ _ __ 
+ `--. \/ __/ _` | '_ \| '_ \ / _ \ '__|
+/\__/ / (_| (_| | | | | | | |  __/ |   
+\____/ \___\__,_|_| |_|_| |_|\___|_|   
+                                       
+                                       """)
+
+                                       
 
 print("*****************************************************************")
-print("* ╔╦╗╦═╗  ╔═╗╦═╗╔═╗═╗ ╦╦ ╦                                      *")
-print("* ║║║╠╦╝  ╠═╝╠╦╝║ ║╔╩╦╝╚╦╝                                      *")
-print("* ╩ ╩╩╚═  ╩  ╩╚═╚═╝╩ ╚═ ╩                                       *")
 print("* Copyright of Mr.Proxy, 2021                                   *")
 print("* https://github.com/Mr-Proxy-source                            *")
 print("* https://www.youtube.com/c/MrProxy1                            *")
